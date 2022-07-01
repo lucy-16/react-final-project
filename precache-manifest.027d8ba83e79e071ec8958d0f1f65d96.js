d18de0ff11afedce2d203dcff2455ea0eb61251c
Lucy Varshitha <lucyvarshithabellam@gmail.com>
2022-07-01 23:43:56 +0530

    Updates

---
+++ precache-manifest.027d8ba83e79e071ec8958d0f1f65d96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faca362f76111a4d1c0aa29aa7bc5775",
+    "url": "/react-final-project/index.html"
+  },
+  {
+    "revision": "2294fff74165c9962e52",
+    "url": "/react-final-project/static/css/2.1ebb0010.chunk.css"
+  },
+  {
+    "revision": "21b735f7ff89030acf0f",
+    "url": "/react-final-project/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "2294fff74165c9962e52",
+    "url": "/react-final-project/static/js/2.cdd8d94b.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/react-final-project/static/js/2.cdd8d94b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b735f7ff89030acf0f",
+    "url": "/react-final-project/static/js/main.0ffb25f3.chunk.js"
+  },
+  {
+    "revision": "38fb742eb66703d28c04",
+    "url": "/react-final-project/static/js/runtime-main.21476bbf.js"
+  },
+  {
+    "revision": "fb08f8fed4141719402d79073c478131",
+    "url": "/react-final-project/static/media/cart.fb08f8fe.svg"
+  }
+]);
