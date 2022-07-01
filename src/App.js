f7a18f384a8be83b2dc50ec7927b9e4092983d2d
Lucy Varshitha <lucyvarshithabellam@gmail.com>
2022-07-01 23:34:57 +0530

    built the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+import { Container } from 'reactstrap';
+import Navbar from './components/Navbar';
+import { CartProvider } from './lib/cart.context';
+import Category from './pages/Category';
+import Checkout from './pages/Checkout';
+import Home from './pages/Home';
+
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      <Container>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/category/:id" exact>
+            <Category />
+          </Route>
+          <Route path="/checkout" exact>
+            <Checkout />
+          </Route>
+        </Switch>
+      </Container>
+    </CartProvider>
   );
 }
 
